d2011759fbb8b81e63a992f420e4da6b3bfe4d09
hyeals <48932621+hyeals@users.noreply.github.com>
2020-04-23 10:34:04 +0900

    Create 1436.kt

---
+++ 단계별/1436.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    var src = Scanner(System.`in`)
+    var input : Int = src.nextInt()
+    var flag: Int = 0
+    var number: String = "0"
+
+    while(input!=flag){
+
+        number = (number.toInt()+1).toString()
+
+        if("666" in number) flag ++
+    }
+
+    println(number)
+}
